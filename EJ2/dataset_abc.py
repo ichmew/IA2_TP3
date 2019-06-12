362a4ce33edb09b6ea1f6a352b853766d6c75c05
ichmew <31658773+ichmew@users.noreply.github.com>
2019-06-12 20:29:30 -0300

    Update dataset_abc.py

--- EJ2/dataset_abc.py
+++ EJ2/dataset_abc.py
@@ -13,9 +13,20 @@ def genera_data():
 
     return labels, dataset
 
-    
+
 def dataset_size():
     datos = pd.read_csv('dataset_reducido.csv', header=0, na_filter=False)
     cant_datos = datos.shape[0]  # shape[0] para filas, shape[1] para columnas
     return cant_datos
 
+
+def genera_data_inicializacion():
+    datos = pd.read_csv('dataset_inicializacion.csv', header=0, na_filter=False)
+
+    labels = np.zeros(datos.shape[0])
+    dataset = np.zeros([datos.shape[0], datos.shape[1] - 1])
+    for i in range(0, datos.shape[0]):
+        labels[i] = datos.iloc[i][0]
+        dataset[i][:] = datos.iloc[i][1:]
+
+    return labels, dataset
