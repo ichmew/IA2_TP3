30321cde04133c7850871d0b86fed9d4f9cac5d2
gabrielquiroga <31658998+gabrielquiroga@users.noreply.github.com>
2019-06-18 16:44:10 -0300

    Update lvq.py

--- EJ2/lvq.py
+++ EJ2/lvq.py
@@ -13,12 +13,13 @@ from dataset_abc import genera_data
 from dataset_abc import dataset_size
 from dataset_abc import genera_data_inicializacion
 
-h = 0.1
+h = 0.5
 NEURONAS_ENTRADA = 28*28
-MAPA_X = 25
-MAPA_Y = 25
+MAPA_X = 28
+MAPA_Y = 28
 MAPA_Z = NEURONAS_ENTRADA
 NEURONAS_MAPA = MAPA_X*MAPA_Y
+PORC_EJ_CLAS = 0.3
 PORC_EJ_TEST = 0.1
 EPOCHS = 1
 #Parece que lo de hacer un alfa variable en el tiempo es sumamente importante, 
@@ -36,17 +37,23 @@ print('Antes del genera_data')
 labels, dataset = genera_data()
 print('Pasó el genera_data')
 
-CANT_EJ = dataset_size() - 27500
+CANT_EJ = dataset_size() - 19000
 CANT_EJ_TEST = int(CANT_EJ * PORC_EJ_TEST)
-CANT_EJ_CLAS = 100
+CANT_EJ_CLAS = int(CANT_EJ * PORC_EJ_CLAS)
 CANT_EJ_TRAINING = CANT_EJ - CANT_EJ_TEST - CANT_EJ_CLAS
 
-ALFAi = 10
-ALFAf = 0.01
+#ALFA = 10
+ALFAi = 2
+ALFAf = 0.005
 tALFA = CANT_EJ_TRAINING
 def n_alfa(mu):
     ALFA = ALFAi*pow(ALFAf/ALFAi, mu/tALFA)
     return ALFA
+
+# def n_alfa_lineal(mu):
+#     ALFA = ALFAi + (ALFAf - ALFAi)*(mu/tALFA)
+#     return ALFA
+
 
 def calculo_metrica(Wentrada, Wmapa):
     # Calcula la distancia euclideana entre 2 vectores de pesos
@@ -84,7 +91,7 @@ def aprendizaje(Wentrada, Wijk):
     min_x, min_y = comparacion(Wentrada, Wijk)
     for i in range(min_x - 1, min_x + 2):
         for j in range(min_y - 1, min_y + 2):
-            '''
+            #'''
             # Para las neuronas de los bordes:
             if min_x==0 and min_y>0 and min_y<MAPA_Y-1:
                 if i == min_x and j == min_y:
@@ -126,10 +133,14 @@ def aprendizaje(Wentrada, Wijk):
                 else:
                     for k in range(0, NEURONAS_ENTRADA):
                         Wijk[i][j][k] += ALFA*(Wentrada[k] - Wijk[i][j][k])*h
-            else:'''
-            if i>=0 and i<MAPA_X and j>=0 and j<MAPA_Y:
+            else:
+                #'''
+                #if i>=0 and i<MAPA_X and j>=0 and j<MAPA_Y:
                 for k in range(0, NEURONAS_ENTRADA):
-                    Wijk[i][j][k] += ALFA*(Wentrada[k] - Wijk[i][j][k])
+                    if  i == min_x and j ==min_y:
+                        Wijk[i][j][k] += ALFA*(Wentrada[k] - Wijk[i][j][k])
+                    else:
+                        Wijk[i][j][k] += ALFA*(Wentrada[k] - Wijk[i][j][k])*h
 
     return Wijk, min_x, min_y
 
@@ -139,17 +150,18 @@ print('Entrenando...')
 for e in range(0, EPOCHS):
     for mu in range(0, CANT_EJ_TRAINING):
         ALFA = n_alfa(mu)
+        # h = n_vecindad(mu)
         Wentrada = dataset[mu][:]
         Wijk, min_x, min_y = aprendizaje(Wentrada, Wijk)
         mapa[min_x, min_y] += 1
         print(mu)
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.set_aspect('equal')
-plt.imshow(mapa, interpolation='nearest', cmap=plt.cm.ocean)
-plt.colorbar()
-plt.show()
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1)
+# ax.set_aspect('equal')
+# plt.imshow(mapa, interpolation='nearest', cmap=plt.cm.ocean)
+# plt.colorbar()
+# plt.show()
 
 
 
@@ -179,24 +191,33 @@ for mu in range(CANT_EJ_TRAINING, CANT_EJ_TRAINING + CANT_EJ_CLAS):
         for k in range(0, MAPA_Z):
             Wijk[min_x][min_y][k] = Wijk[min_x][min_y][k] - ALFA*(Wentrada[k] - Wijk[min_x][min_y][k])
         # print('Error: ', clase_patron, ' ', clase_ganadora)
+    print(mu)
 # print(labels_map)
 # print(tasa_de_aciertos)
 
 # TEST-----------------------------------------------------------------------------
-tasa_de_aciertos = 0
-tasa_de_errores = 0
+cantidad_de_aciertos = 0
+cantidad_de_errores = 0
 for mu in range(CANT_EJ_TRAINING + CANT_EJ_CLAS, CANT_EJ_TRAINING + CANT_EJ_CLAS + CANT_EJ_TEST):
     Wentrada = dataset[mu][:]
     clase_patron = labels[mu]
     min_x, min_y = comparacion(Wentrada, Wijk)
     clase_ganadora = labels_map[min_x, min_y]
     if clase_ganadora == clase_patron:
-        tasa_de_aciertos += 1
+        cantidad_de_aciertos += 1
         print('Acierto: ', clase_patron, ' ', clase_ganadora, '<----')
     else:
-        tasa_de_errores += 1
+        cantidad_de_errores += 1
         print('Error: ', clase_patron, ' ', clase_ganadora)
 
 print(labels_map)
-print('Cantidad de aciertos: ', tasa_de_aciertos)
-print('Cantidad de errores: ', tasa_de_errores)
+print('Cantidad de aciertos: ', cantidad_de_aciertos)
+# print('Cantidad de errores: ', cantidad_de_errores)
+print('Ejemplos de test: ', CANT_EJ_TEST)
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.set_aspect('equal')
+plt.imshow(mapa, interpolation='nearest', cmap=plt.cm.ocean)
+plt.colorbar()
+plt.show()
